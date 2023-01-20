9085cf7cbe53679063ac391bbb3facbe1d4e13a5
Artem Lira <artemlira84@gmail.com>
2023-01-20 13:04:39 +0200

    added pictures and animations to the Testimony section

--- src/components/Page_Home/Testimony/Testimony.js
+++ src/components/Page_Home/Testimony/Testimony.js
@@ -2,48 +2,45 @@ import React, { forwardRef } from 'react';
 import { motion } from 'framer-motion';
 import styles from './Testimony.module.scss';
 import logo from '../../../images/Hero/iconLogo.svg';
-
+import { isWebpSupported } from 'react-image-webp/dist/utils';
 
 const animation = {
   hidden: {
-    x: "100%",
-    scale: 0.3,
+    y: 100,
+    opacity: 0,
   },
-  visible: {
-    x: 0,
-    scale: 1,
-    transition: { duration: 1 }
-  },
-  exit: {
-    x: "100%",
-  }
+  visible: custom => ({
+    y: 0,
+    opacity: 1,
+    transition: { delay: custom * 0.1 }
+  })
 }
 
 const testimonyBD = [
   {
-    photo: '',
-    photoWebp: '',
+    photo: '../../../images/Testimony/01.jpg',
+    photoWebp: '../../../images/Testimony/01.webp',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing sit. auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue.',
     author: 'Wade Warren',
     position: 'Sommelier',
   },
   {
-    photo: '',
-    photoWebp: '',
+    photo: '../../../images/Testimony/02.jpg',
+    photoWebp: '../../../images/Testimony/02.webp',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing sit. auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue.',
     author: 'Jane Cooper',
     position: 'Chef',
   },
   {
-    photo: '',
-    photoWebp: '',
+    photo: '../../../images/Testimony/03.jpg',
+    photoWebp: '../../../images/Testimony/03.webp',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing sit. auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue.',
     author: 'Robert Fox',
     position: 'Chef',
   },
   {
-    photo: '',
-    photoWebp: '',
+    photo: '../../../images/Testimony/04.jpg',
+    photoWebp: '../../../images/Testimony/04.webp',
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing sit. auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget sit. Nulla scelerisque scelerisque congue.',
     author: 'Brooklyn Simmons',
     position: 'Caterer',
@@ -64,12 +61,16 @@ export default function Testimony() {
           <div className={styles.contant}>
             <motion.ul
               initial='hidden'
-              animate="visible"
+              whileInView="visible"
+              viewport={{ amount: 0.2, once: true }}
               className={styles.contantList}>
-              {testimonyBD.map(el =>
+              {testimonyBD.map((el, i) =>
                 <MTestimonyItem
+                  custom={i + 1}
+                  variants={animation}
                   key={Math.random() * 10}
                   photo={el.photo}
+                  photoWebp={el.photoWebp}
                   text={el.text}
                   author={el.author}
                   position={el.position}
@@ -85,7 +86,9 @@ export default function Testimony() {
 const TestimonyItem = forwardRef(({ photo, photoWebp, text, author, position }, ref) => (
   <li className={styles.contantItem} ref={ref}>
     <div className={styles.itemPhoto}>
-
+      {isWebpSupported()
+        ? <img src={photoWebp} alt='author' />
+        : <img src={photo} alt='author' />}
     </div>
     <div className={styles.itemContant}>
       <p className={styles.itemText}>{text}</p>
