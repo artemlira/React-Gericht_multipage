650dc5a998ceb52ba0e8c9058275aac8c4c8d34b
Artem Lira <artemlira84@gmail.com>
2023-02-05 23:25:36 +0200

    finished the Histiry suction on the About page

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -6,15 +6,15 @@ import styles from './Button.module.scss';
 Button.propTypes = {
   text: PropTypes.string.isRequired,
   className: PropTypes.string,
-  link: PropTypes.string
+  link: PropTypes.string,
+  anchor: PropTypes.string
 };
-export default function Button ({ text, className = false, link = false }) {
+export default function Button ({ text, className = false, link = false, anchor = false }) {
   return (
     <button className={className ? `${styles.button} ${className}` : `${styles.button}`}>
-      {link.valueOf('#')
-        ? <a href={link}>{text}</a>
-        : <Link to={link ? `${link}` : '/'}>{text}</Link>
-      }
+      {anchor && <a href={anchor}>{text}</a>}
+      {link && <Link to={link}>{text}</Link>}
+      {!link && !anchor && <Link to="/">{text}</Link>}
     </button>
   );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,11 +3,13 @@ import Hero from '../components/Page_Contacts/Hero/Hero';
 import PhotoGallery from './../components/Page_Home/PhotoGallery/PhotoGallery';
 import Testimony from './../components/Page_Home/Testimony/Testimony';
 import Chefs from './../components/Page_Home/Chefs/Chefs';
+import History from './../components/Page_About/History/History';
 
 export default function About () {
   return (
     <article>
       <Hero title="Welcome to Gerícht" page="About Us" />
+      <History />
       <Chefs />
       <Testimony />
       <PhotoGallery />
